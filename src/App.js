c16f6b165fb359c4420d71e579681c3810d5ebb2
Aman Chopra <amanchopra.atg@gmail.com>
2021-06-11 11:33:55 +0530

    I am Contributor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Content from "./Content";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <br /><br /><br /><br />
+      <h2 style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>This is a demo webpage for Practicing Open Source 😉</h2>
+      <br /><br />
+      <Content />
+      <hr />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Content.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Cards from './Cards'
+
+const Content = () => {
+  return (
+    <div>
+
+      <Cards
+        sname="Aman Chopra"
+        skill1="Web Developer (React)"
+        skill2="Cloud (AWS)"
+        skill3="DevOps"
+        skill4="Java"
+        l_link="https://www.linkedin.com/in/aman-chopra-375a81171/"
+        g_link="https://github.com/Aman1905"
+        t_link ="https://twitter.com/iam_chopra_aman" />
+
+    </div>
+  )
+}
+
+export default Content
