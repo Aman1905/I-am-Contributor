653a1c470c0d230c1a6feae0dc91a208f9d3e0b0
prasanthgrandhe <84303639+prasanthgrandhe@users.noreply.github.com>
2021-06-21 21:01:52 +0530

    testing
    updated card

--- src/Content.js
+++ src/Content.js
@@ -64,6 +64,15 @@ const Content = () => {
         l_link="https://www.linkedin.com/in/nilisha-jaiswal-7980061b2/"
         g_link="https://github.com/nilisha-jais"
         t_link ="https://twitter.com/jaiswal_nilu05" />
+    <Cards
+        sname="Prasanth Grandhe"
+        skill1="Web Developer"
+        skill2="C+"
+        skill3="Js"
+        skill4="HTML"
+        l_link="www.linkedin.com/in/grandheprasanth"
+        g_link="https://github.com/prasanthgrandhe"
+        t_link ="" />
 
 
     </div>
